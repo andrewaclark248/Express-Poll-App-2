a96e28ace0b8cabaa200d97eadedf5c420b6575c
andrew <andrewacalrk248@gmail.com>
2022-01-02 10:46:48 -0500

    add comments

---
+++ services/loginService.js
@@ -0,0 +1,31 @@
+//register
+
+//authenticate
+function register( username, password ) {
+    //create users
+    //check if user already exists?
+
+    //yes
+        //return error
+    //no
+        //add user to db
+        //create JWT
+
+}
+
+
+//login
+function login(user) {
+    //check if user exists
+    
+    //yes user exist
+        // then add JWT
+        // add JWT through loginService
+    //no user does not exist
+        //return error
+}
+
+
+module.exports = {
+    register
+};
