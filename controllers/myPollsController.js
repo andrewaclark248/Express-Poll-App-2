5c621aad139722643eddc090eb8425d1d3a3e250
andrew <andrewacalrk248@gmail.com>
2022-01-09 00:50:51 -0500

    find question

--- controllers/myPollsController.js
+++ controllers/myPollsController.js
@@ -34,6 +34,18 @@ async function edit (req, resp) {
 
 async function update (req, resp) {
     debugger
+    var answers = req.body
+    for (var a in answers) {
+        var question_id = Number(a.slice(-2))
+        var question = await models.Questions.findOne({
+            where: {
+                id: question_id
+            }
+        });
+        //answers[a]
+        //update answer
+        debugger
+    }
     resp.render("myPolls/edit");
 }
 
