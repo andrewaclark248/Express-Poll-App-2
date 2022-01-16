d4b8fba324ec5dfe90c1a0cc88cdbf229f7284b3
andrew <andrewacalrk248@gmail.com>
2022-01-16 16:03:20 -0500

    test login service

--- services/loginUser.js
+++ services/loginUser.js
@@ -6,14 +6,27 @@ const KEY = "mysomethingkey"
 class LoginUser extends Interactor {
     static async run(context) {
     var token = null;
+
+    if (context.email.length == 0){
+        context.error = "Please provide a valid email"
+    }
+    if (context.password.length==0) {
+        context.error = "Please provide a password"
+    }
+    
+    if (context.error != null){
+        return {error: context.error}
+    }
+
     try{
         var user = await models.User.findOne({ where: { userName: context.email } });
-        
+        debugger
         if (user){
             if(user.password == context.password)
             {
                 token = jwt.sign(user.toJSON(), KEY, { expiresIn: "1h" });
                 context.isAdmin  = (user.role == models.User.ADMIN_ROLE) ? true : false;
+                
             }
 
         } else
